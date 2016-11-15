838175a208197635218bbd9d3dd243cfd30ab271
bryan juarez <bj910052@gmail.com>
2016-11-15 10:18:43 -0800

    more work
    added shapes to the js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,3 +1,12 @@
 // Write all your JavaScript code in this file!
 // You can use the namespace variable that's defined here.
 var namespace = "http://www.w3.org/2000/svg"
+function makeshape(){
+   var circle =document.createElementNS(namespace,"circle")
+   circle.setAttribute("cx",50 )
+  circle.setAttribute("cy", 50)
+ circle.setAttribute("r",30 )
+var canvas = document.getElementById("game-board")
+canvas.appendChild(circle)
+
+}
