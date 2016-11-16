6f7f8a41b3b0fae565db0bfc8af8af06a503fca7
bryan juarez <bj910052@gmail.com>
2016-11-16 10:19:11 -0800

    FIXED game piece
    caps lock was on my bad but ya i added and fixed the game piece

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,9 +3,9 @@
 var namespace = "http://www.w3.org/2000/svg"
 function makeshape(){
    var circle =document.createElementNS(namespace,"circle")
-   circle.setAttribute("cx",50 )
-  circle.setAttribute("cy", 50)
- circle.setAttribute("r",30 )
+   circle.setAttribute("cx",55 )
+  circle.setAttribute("cy", 55)
+ circle.setAttribute("r",45 )
 var canvas = document.getElementById("game-board")
 canvas.appendChild(circle)
 
